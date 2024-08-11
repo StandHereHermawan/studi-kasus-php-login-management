4134edf1f9e6ee0cc2aeb634762e65ebf472c296
Arief-Karditya <nolifegansaya@gmail.com>
2024-08-11 21:45:28 +0700

    User Password Update Service

---
+++ 35-user-password-update-service/project/public/index.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+use AriefKarditya\LocalDomainPhp\App\Router;
+use AriefKarditya\LocalDomainPhp\Config\Database;
+use AriefKarditya\LocalDomainPhp\Controller\HomeController;
+use AriefKarditya\LocalDomainPhp\Controller\UserController;
+use AriefKarditya\LocalDomainPhp\Middleware\MustLoginMiddleware;
+use AriefKarditya\LocalDomainPhp\Middleware\MustNotLoginMiddleware;
+
+Database::getConnection('prod');
+
+# Home Controller
+Router::add('GET', '/', HomeController::class, 'index', []);
+
+# User Controller Must Not Login
+Router::add('GET', '/users/register', UserController::class, 'register', [MustNotLoginMiddleware::class]);
+Router::add('POST', '/users/register', UserController::class, 'postRegister', [MustNotLoginMiddleware::class]);
+Router::add('GET', '/users/login', UserController::class, 'login', [MustNotLoginMiddleware::class]);
+Router::add('POST', '/users/login', UserController::class, 'postLogin', [MustNotLoginMiddleware::class]);
+
+# User Controller Must Login
+Router::add('GET', '/users/logout', UserController::class, 'logout', [MustLoginMiddleware::class]);
+Router::add('GET', '/users/profile', UserController::class, 'updateProfile', [MustLoginMiddleware::class]);
+Router::add('POST', '/users/profile', UserController::class, 'postUpdateProfile', [MustLoginMiddleware::class]);
+
+Router::run();
+
+# jika URL = localhost:8080/login, maka path-nya
+# require __DIR__ . '/../app/View/login.php";
+
+# localhost:8080/index.php/category = localhost:8080/category. $_SERVER['PATH_INFO'] = /category
+# localhost:8080/index.php/user/login = localhost:8080/user/login. $_SERVER['PATH_INFO'] = /user/login
+
+# localhost:8080/index.php/category?name=gadget = localhost:8080/category?name=gadget. $_SERVER['PATH_INFO'] = /category
+# localhost:8080/index.php/product?name=gadget = localhost:8080/product?name=gadget. $_SERVER['PATH_INFO'] = /product
+# query parameter ga masuk ke path info.
